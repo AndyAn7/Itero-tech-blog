3a8614a9359c44ae27508e64e23b3572212e5dce
Andrew An <andyan7@gmail.com>
2022-04-03 11:21:48 -0700

    comment.js

---
+++ public/js/comment.js
@@ -0,0 +1,24 @@
+const commentFormHandler = async (event) => {
+    event.preventDefault();
+
+    const post_comment = document.querySelector('textarea[name="post-comment"]').value.trim();
+    const post_id = window.location.toString().split('/')[window.location.toString().split('/').length - 1];
+
+    if (post_comment) {
+        const response = await fetch('/api/comment', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                post_comment,
+                post_id
+            })});
+
+            if (response.ok) {
+                document.location.reload();
+            } else {
+                alert('Something went wrong');
+            }}};
+
+document.querySelector('.comment-form').addEventListener('submit', commentFormHandler);
