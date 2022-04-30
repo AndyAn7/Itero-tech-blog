832009ba4b947b21647b7f195a09eea92c7e4ab1
Andrew An <andyan7@gmail.com>
2022-04-30 14:04:44 -0700

    fixed posts

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -12,7 +12,7 @@ const newFormHandler = async (event) => {
         });
 
         if (response.ok) {
-            document.location.replace = ('/dashboard');
+            document.location.replace('/dashboard');
         } else {
             alert('Something went wrong');
         }}};
